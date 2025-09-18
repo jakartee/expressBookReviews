177ecc3a3401202603de8fd7b7d6b886c4b977de
jakartee <151826707+jakartee@users.noreply.github.com>
2025-09-18 12:58:07 +0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,23 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  if(req.session){
+    if(req.session.authorization){
+      let token = req.session.authorization.token;
+      
+      jwt.verify(token, 'my-secret-key', (err, user) => {
+        if (err) {
+            res.status(401).json({ error: 'galek err' });
+        } else {
+          req.session.user = user;
+          next();
+        }
+      });
+    } else {
+      res.status(401).json({ error: 'Unauthorized here' });
+    }
+  }
 });
  
 const PORT =5000;
